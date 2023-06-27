a3d0ecae037f80497b854c5cab085f42ed416067
claudia-sikorski <c.sikorski@outlook.de>
2023-06-27 18:28:51 +0200

    save interim status

--- src/components/QuizWrapper.jsx
+++ src/components/QuizWrapper.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ListQuiz from "./ListQuiz";
 
 const QuizWrapper = () => {
-  return <div></div>;
+  return (
+    <div className="quizContainer">
+      <ListQuiz />
+    </div>
+  );
 };
 
 export default QuizWrapper;
