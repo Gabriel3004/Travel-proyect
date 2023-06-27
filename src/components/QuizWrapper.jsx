00532ab4e1ef28ece3787579103cea1b781706eb
claudia-sikorski <c.sikorski@outlook.de>
2023-06-27 16:31:34 +0200

    added main structure

---
+++ src/components/QuizWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuizWrapper = () => {
+  return <div></div>;
+};
+
+export default QuizWrapper;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QuizWrapper from "./components/QuizWrapper";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QuizWrapper />
+  </React.StrictMode>
+);
