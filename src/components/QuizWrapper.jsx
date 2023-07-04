4a8d6fcf478343b85d9a3ff22b930a0097ed1e52
uaeboss <jeskemal@gmail.com>
2023-07-04 14:45:17 +0200

    added result display

--- src/components/QuizWrapper.jsx
+++ src/components/QuizWrapper.jsx
@@ -5,8 +5,9 @@ import Questions from "./Questions";
 const QuizWrapper = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [result, setResult] = useState({
-    score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
   });
@@ -15,43 +16,82 @@ const QuizWrapper = () => {
   const { question, answers, correctAnswer } = questions[currentQuestion];
 
   const onClickNext = () => {
-    setCurrentQuestion((prev) => prev + 1);
+    setSelectedAnswerIndex(null);
     setResult((prev) =>
       selectedAnswer
         ? {
             ...prev,
-            score: prev.score + 10,
             correctAnswers: prev.correctAnswers + 1,
           }
-        : { ...prev,
-          wrongAnswers: prev.wrongAnswers + 1 }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
     );
-  };
-
-  const onAnswerSelected = (answer) => {
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true);
-      console.log("right");
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
     } else {
-      setSelectedAnswer(false);
-      console.log("false");
+      setCurrentQuestion(0);
+      setShowResult(true);
     }
   };
 
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+    } else {
+      setSelectedAnswer(false);
+    }
+  };
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
 
   return (
     <div className="wrapper">
       <div className="container">
-        <h1>Trivia</h1>
-        <h2>{question}</h2>
-        <ul>
-          {answers.map((answer) => (
-            <div className="answers"><li onClick={() => onAnswerSelected(answer)} key={answer}>
-              {answer}
-            </li></div>
-          ))}
-        </ul>
-        <button onClick={onClickNext}>Next</button>
+        {!showResult ? (
+          <div>
+            <div></div>
+            <h2>{question}</h2>
+            <ul>
+              {answers.map((answer, index) => (
+                <li
+                  onClick={() => onAnswerSelected(answer, index)}
+                  key={answer}
+                  className={
+                    selectedAnswerIndex === index ? "selected-answer" : null
+                  }
+                >
+                  {answer}
+                </li>
+              ))}
+            </ul>
+            <div className="flex">
+              <div>
+                <span className="active-question-no">
+                  {addLeadingZero(currentQuestion + 1)}
+                </span>
+                <span className="total-question">
+                  /{addLeadingZero(questions.length)}
+                </span>
+              </div>
+              <div className="flex-right">
+                <button
+                  onClick={onClickNext}
+                  disabled={selectedAnswerIndex === null}
+                >
+                  {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="result">
+            <h3>Your result:</h3>
+            <div>
+              You answered {result.correctAnswers}/{questions.length} questions
+              correctly
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
